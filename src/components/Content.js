82a161505b504e9b2700d89c37c6c1069c726a86
Esma Haber <esma.haber@gmail.com>
2021-12-26 01:18:57 +0300

    content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,29 @@
 import Header from "./Header/Header";
 import Brands from "../brands.json";
+import { useState } from "react/cjs/react.development";
+import Brand from "./Brand";
 
 function Content() {
+
+    const brandsArray = [];
+
+    Object.keys(Brands).map(key => {
+        brandsArray.push(Brands[key])
+    })
+
+    const [brands, setBrands] = useState(brandsArray);
+
     return ( 
     <>
-    <main className="content">
-        <Header/>
-    </main>
-    <section>
-
-    </section>
+        <main className="content">
+            <Header/>
+            <section className="brands">
+                {brands.map(item => (
+                     <Brand brand={item}/>
+                )
+                )}
+            </section>
+        </main>
     </>
 
      );
